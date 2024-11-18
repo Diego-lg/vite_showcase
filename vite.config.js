3f158d8e1ab71a2464fbe4714eb35c61fc2f033a
Diego-lg <diegoligarrido@gmail.com>
2024-11-18 00:50:38 -0800

    fixed issues related to logo positioning

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import path from "path"; // Import path
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "./", // Optional: Set to '/' for root
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // Set alias for "@/..." to point to "./src"
